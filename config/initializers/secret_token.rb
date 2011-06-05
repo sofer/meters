8e59c54e4bba31d26f7c299b90a4f28ea2e348ed
D Sofer <d@sofer.com>
2011-06-05 23:11:28 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meters::Application.config.secret_token = '67297e05af900e39a435942428cc48b3ba3c2aa2184c5e6d720a09d271127b4206d6e2a1d2236d54ee09420f38b5288911149d9dd16bf0deab1187f285d808b6'
